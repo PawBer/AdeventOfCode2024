6e121d4faac58829a9257770960d4ea529b15b04
Paweł Bernaciak <pawelbernaciak@zohomail.eu>
2024-12-01 11:40:16 +0100

    Day 1

---
+++ Day1Part1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Day1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> firstList = [], secondList = [];
+        
+        foreach (var line in File.ReadLines("input.txt"))
+        {
+            var numbers = line.Split("   ");
+            firstList.Add(int.Parse(numbers[0]));
+            secondList.Add(int.Parse(numbers[1]));
+        }
+        
+        firstList.Sort();
+        secondList.Sort();
+        
+        var distances = firstList.Zip(secondList, (a, b) => Math.Abs(a-b));
+        Console.WriteLine(distances.Sum());
+    }
+}
---
+++ Day1Part2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Day1Part2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> firstList = [], secondList = [];
+        
+        foreach (var line in File.ReadLines("input.txt"))
+        {
+            var numbers = line.Split("   ");
+            firstList.Add(int.Parse(numbers[0]));
+            secondList.Add(int.Parse(numbers[1]));
+        }
+        
+        var numberCounts = new Dictionary<int, int>();
+        foreach (var number in secondList)
+        {
+            numberCounts.TryAdd(number, 0);
+            numberCounts[number]++;
+        }
+
+        var similarity = 0;
+        foreach (var number in firstList)
+        {
+            if (numberCounts.TryGetValue(number, out var count))
+            {
+                similarity += count * number;
+            }
+        }
+        
+        Console.WriteLine(similarity);
+    }
+}
