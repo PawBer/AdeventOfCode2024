a299d8c2c48c1fc96fba98962066b389f855c7bd
Paweł Bernaciak <pawelbernaciak@zohomail.eu>
2024-12-02 15:43:25 +0100

    Day 2

---
+++ Day2Part1/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace Day2Part1;
+
+struct Report
+{
+    private List<int> _measurements;
+
+    public Report(string reportString)
+    {
+        var measurements = reportString.Split(' ').Select(int.Parse).ToList();
+        _measurements = measurements;
+    }
+
+    public bool IsMonotonic()
+    {
+        if (_measurements.Count < 2)
+        {
+            return true;
+        }
+
+        var monotonic = true;
+        if (_measurements[0] < _measurements[1])
+        {
+            for (var i = 1; i < _measurements.Count - 1; i++)
+            {
+                if (_measurements[i] > _measurements[i + 1])
+                {
+                    monotonic = false;
+                }
+            }
+        }
+        else
+        {
+            for (var i = 1; i < _measurements.Count - 1; i++)
+            {
+                if (_measurements[i] < _measurements[i + 1])
+                {
+                    monotonic = false;
+                }
+            }
+        }
+
+        return monotonic;
+    }
+
+    public bool IsChangingSafely()
+    {
+        if (_measurements.Count < 2)
+        {
+            return true;
+        }
+
+        var safe = true;
+        for (var i = 0; i < _measurements.Count - 1; i++)
+        {
+            if (_measurements[i] == _measurements[i + 1] || Math.Abs(_measurements[i] - _measurements[i + 1]) > 3)
+            {
+                safe = false;
+            }
+        }
+
+        return safe;
+    }
+    
+    public bool IsSafe() => IsMonotonic() && IsChangingSafely();
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var reports = File.ReadAllLines("input.txt").Select((line) => new Report(line)).ToList();
+        var sum = reports.Count(r => r.IsSafe());
+        Console.WriteLine(sum);
+    }
+}
---
+++ Day2Part2/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace Day2Part2;
+
+static class Report
+{
+    public static bool IsMonotonic(List<int> measurements)
+    {
+        if (measurements.Count < 2)
+        {
+            return true;
+        }
+
+        var monotonic = true;
+        if (measurements[0] < measurements[1])
+        {
+            for (var i = 1; i < measurements.Count - 1; i++)
+            {
+                if (measurements[i] > measurements[i + 1])
+                {
+                    monotonic = false;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            for (var i = 1; i < measurements.Count - 1; i++)
+            {
+                if (measurements[i] < measurements[i + 1])
+                {
+                    monotonic = false;
+                    break;
+                }
+            }
+        }
+
+        return monotonic;
+    }
+
+    public static bool IsChangingSafely(List<int> measurements)
+    {
+        if (measurements.Count < 2)
+        {
+            return true;
+        }
+
+        var safe = true;
+        for (var i = 0; i < measurements.Count - 1; i++)
+        {
+            if (measurements[i] == measurements[i + 1] || Math.Abs(measurements[i] - measurements[i + 1]) > 3)
+            {
+                safe = false;
+                break;
+            }
+        }
+
+        return safe;
+    }
+
+
+    public static bool IsSafe(List<int> measurements) => IsMonotonic(measurements) && IsChangingSafely(measurements);
+
+    public static bool CheckWithTolerance(List<int> measurements)
+    {
+        if (IsSafe(measurements))
+        {
+            return true;
+        }
+
+        for (var i = 0; i < measurements.Count; i++)
+        {
+            var measurementsCopy = measurements.ToList();
+            measurementsCopy.RemoveAt(i);
+            if (IsSafe(measurementsCopy))
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var reports = File
+            .ReadAllLines("input.txt")
+            .Select(line => line
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList()
+            )
+            .ToList();
+
+        var safeReports = reports.Count(Report.CheckWithTolerance);
+        Console.WriteLine(safeReports);
+        //reports.Select(Report.CheckWithTolerance).ToList().ForEach(Console.WriteLine);
+    }
+}
